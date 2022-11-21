a2ae33d5251e0643c232df7645f31d2d402b1bbc
Paul Pruenster <paul.pruenster@gmail.com>
2022-11-21 12:00:56 +0100

    2c order by

--- sheet_05/exercise2/c.sql
+++ sheet_05/exercise2/c.sql
@@ -1,4 +1,5 @@
-select * from customer where address_id in (
+select c.first_name, c.last_name from customer c where address_id in (
 	select address_id from address where city_id in (
 		select city_id from city where country_id in (
-			select country_id from country c where c.country like '%land'))) 
+			select country_id from country where country like '%land')))
+				order by c.first_name, c.last_name
