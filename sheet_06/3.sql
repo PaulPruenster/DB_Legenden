3fe9524d168661b1e2da2c6972ca28a0e1cb09a6
Paul Pruenster <paul.pruenster@gmail.com>
2022-11-28 14:15:05 +0100

    3 vlt folsch

---
+++ sheet_06/3.sql
@@ -0,0 +1,19 @@
+select f.title, c."name" as category_name, sum(p.amount) as total_earnings, avg(p.amount) as average_payment from film f
+ join inventory i on f.film_id = i.film_id
+ join rental r on i.inventory_id = r.inventory_id
+ join payment p on r.rental_id = p.rental_id
+ join film_category fc on fc.film_id = f.film_id 
+ join category c on c.category_id = fc.category_id 
+group by f.title, c."name"  
+
+union all 
+
+select 'Category Pricings' as title, c."name" as category_name, sum(p.amount) as total_earnings, avg(p.amount) as average_payment from category c 
+	join film_category fc on c.category_id = fc.category_id 
+	join film f on f.film_id = fc.film_id 
+ 	join inventory i on f.film_id = i.film_id
+ 	join rental r on i.inventory_id = r.inventory_id
+ 	join payment p on r.rental_id = p.rental_id
+group by c."name"  
+
+order by total_earnings desc, title asc, category_name asc
