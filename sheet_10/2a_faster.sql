7b911fcefff5fcc6696c0321fd0f6861ef827654
Matthias Ebner <matthiasebner491@gmail.com>
2023-01-16 15:19:58 +0100

    change 2

--- sheet_10/2a_faster.sql
+++ sheet_10/2a_faster.sql
@@ -1,7 +1,9 @@
-SELECT first_name, last_name, COUNT(DISTINCT film_id)
+WITH subquery AS (
+  SELECT film_id FROM film WHERE length < 120
+)
+SELECT first_name, last_name, COUNT(DISTINCT film_id) as film_count
 FROM film_actor
-INNER JOIN actor
-ON actor.actor_id = film_actor.actor_id
-WHERE film_id NOT IN (SELECT film_id FROM film WHERE length < 120)
+INNER JOIN actor ON actor.actor_id = film_actor.actor_id
+WHERE NOT EXISTS (SELECT 1 FROM subquery WHERE subquery.film_id = film_actor.film_id)
 GROUP BY first_name, last_name
 ORDER BY first_name, last_name;
